2b64b5b41c8afc001722e35e4eddbcdc48907fa3
maggie-moheb <maggiemoheb@gmail.com>
2015-10-04 21:02:30 +0200

    adding the registering

--- Home.php
+++ Home.php
@@ -5,39 +5,24 @@
     
 	</head>
 	<body>
-  <div style="height: 200px; overflow:hidden; background: url(1.jpg);">
-  <h1 style="color:#fff">Hi</h1>
-		</div>
-    <h1>
-			<?php
-  				 echo "Diagon Alley </br>";
-  				 mysql_connect('localhost', 'root');
-  				 mysql_select_db('Diagon Alley');
-  				 $query = "SELECT * FROM `person`;";
-  				 $result = mysql_query($query) or die(mysql_error());
-  				 $numRows = mysql_num_rows($result);
-  				 if($numRows>0) {
-  				  while($person = mysql_fetch_assoc($result)) {
-  				    echo "<h2>${person['last_name']}</h2>";
-  				  }
-  				 }
-
-  				// echo($numRows);
-			?>
-		</h1>
-    <h1>My First JavaScript</h1>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
-		<form action="LogIn.php" method="POST">
-
- 			Name: <input type="text" name="name" />
-			E-mail: <input type="text" name="email">
- 			<input type="submit" value="Log In"/>
- 		</form>
-
+    <div style="height: 200px; overflow:hidden; background: url(1.jpg);"></div>
+    <div>
+  		<form action="LogIn.php" method="POST">
+        <h3>Log In</h3>
+   			E-mail: <input type="text" name="name" />
+  			Password: <input type="text" name="email">
+   			<input type="submit" value="Log In"/>
+   		</form>
+    </div>
+    <div>
+      <form action="Register.php" method="POST">
+        <h3>Register</h3>
+        First Name: <input type="text" name = "first_name" /></br>
+        E-mail*: <input type="text" name = "email"/> </br>
+        
+        <h5>* required fields</h5>
+        <input type="submit" value="Register"/>
+      </form>
+    </div>
 	</body>
 </html>
