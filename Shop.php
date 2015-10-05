cbec4c6d685af243b3508118a5dc742680770f9d
shrfarzk <shrfarzk@gmail.com>
2015-10-05 23:14:49 +0200

    Added insertion of category names

--- Shop.php
+++ Shop.php
@@ -3,11 +3,11 @@
   mysql_select_db('Diagon Alley');
   $categories = mysql_query("SELECT * FROM category ORDER BY name;") or die(mysql_error());
   $products = array();
-  $numRows = mysql_num_rows($result);
+  $numRows = mysql_num_rows($categories);
   if ($numRows > 0) {
     while ($category = mysql_fetch_assoc($categories)) {
       $categoryProducts = mysql_query("SELECT * FROM product WHERE category_id = ${category['id']} ORDER BY name") or die(mysql_error());
-      array_push($products, array($categorie['name'] => $categoryProducts)); // $categoryProducts is passed by value fa eshta
+      array_push($products, array('name' => $category['name'], 'products' => $categoryProducts));
     }
   }
 ?>
@@ -24,13 +24,40 @@
 <body>
   <header>
     <p class="logo">Diagon Alley</p>
-    <ul>
       <li><a href="#shophome">Shop Home</a></li>
       <li><a href="#history">History</a></li>
-    </ul>
   </header>
   
-  <script>var merchandise = <?=json_encode($products)?>;</script>
+  <aside id="category-list">
+  </aside>
+  
+  <section id="item-section">
+  </section>
+  
+  <!--Scripts-->
+  <script src="js/shop.js"></script>
+  <script>
+    //dummy data
+    var merchandise = [
+      {
+        "name": "Wands",
+        "products": [
+          {
+            "id":"1",
+            "name": "Elder Wand",
+            "description": "Gamda awy ;) gamda awy ;) gamda awy ;) gamda awy ;) gamda awy ;) gamda awy ;)",
+            "summary" : "3ammatan gamda awy",
+            "image" : "img/1.jpg",
+            "price" : "1999.99",
+            "stock" : "1",
+            "category_id" : "mesh khales awy begad"
+          }
+        ]
+      }
+    ];
+    //<?//=json_encode($products)?>;
+    populateShop();
+  </script>
 </body>
 
 </html>
