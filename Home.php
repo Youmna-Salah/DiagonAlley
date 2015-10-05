94a6ae44d4162a9fdca51619c581b5d86a0768fe
maggie-moheb <maggiemoheb@gmail.com>
2015-10-05 16:32:32 +0200

    adding login logic

--- Home.php
+++ Home.php
@@ -3,6 +3,9 @@
   if (array_key_exists('register', $_POST)) {
     register();
   }
+  if(array_key_exists('LogIn', $_POST)) {
+    logIn();
+  }
 ?>
 <html>
   <script type="text/javascript">
@@ -24,7 +27,19 @@
       }
       return true;
     }
-  
+    function checkLoginInput(form) {
+        if(form.email.value == "") {
+          document.getElementById('login_email').innerHTML='Please insert your email';
+          form.email.focus();
+          return false;
+        }
+        if(form.password.value == "") {
+          document.getElementById('login_password').innerHTML='Please insert your password';
+          form.password.focus();
+          return false;
+        }
+        return true;
+    }
   </script>
 	<head>
 		<title>Advanced Lab Project</title>
@@ -33,11 +48,13 @@
 	<body>
     <div style="height: 200px; overflow:hidden; background: url(img/1.jpg);"></div>
     <div>
-  		<form action="LogIn.php" method="POST">
+  		<form onsubmit = "return checkLoginInput(this);" action = "Home.php" method="POST">
         <h3>Log In</h3>
-   			E-mail: <input type="text" name="name" />
-  			Password: <input type="text" name="email">
-   			<input type="submit" value="Log In"/>
+   			E-mail: <input type="text" name="email" />
+        <p id="login_email"></p>
+  			Password: <input type="password" name="password"/>
+        <p id = "login_password"></p>
+   			<input type="submit" value="LogIn" name = "LogIn"/>
    		</form>
     </div>
     <div>
@@ -60,11 +77,9 @@
     </div>
     <?php
       function register() {
-        //echo implode(", ", $_FILES);
-        //echo phpinfo();
         mysql_connect('localhost', "root");
         mysql_select_db('Diagon Alley');
-        $password = password_hash($_POST['password'], PASSWORD_DEFAULT)."\n";
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $query = 'insert into person (first_name, last_name, email, password) 
                  values ("'.$_POST['first_name'].'","'.$_POST['last_name'].'","'.$_POST['email'].'","'.$password.'");';
         $result = mysql_query($query) or die(mysql_error());
@@ -74,20 +89,43 @@
             $target_dir = "Public/ProfilePictures/";
             $target_file = $target_dir . basename($_FILES["image"]["name"]);
             $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-            //echo implode("hhh ",$_FILES);
             if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
               $upload_query = 'update person SET image = "'.$target_file.'" where email= "'.$_POST["email"].'";';
               $result = mysql_query($upload_query) or die(mysql_error());
               if($result) {
+                 session_start();
                 header("Location:Login.php");die;
               }
-              //echo "The file ". basename( $_POST["image"]). " has been uploaded.";
             } else {
               echo "Sorry, there was an error uploading your file.";
             }
           }
           
         }
+      }
+      function logIn() {
+        mysql_connect('localhost', "root");
+        mysql_select_db('Diagon Alley');
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        
+        $query = 'select  password from person where email = "'.$_POST["email"].'" limit 1;';
+        $result = mysql_query($query) or die(mysql_error());
+        $numRows = mysql_num_rows($result);
+        
+        $realRes = implode("",mysql_fetch_assoc($result));
+      
+        if($numRows>0) {
+
+          if(password_verify($_POST['password'], $realRes)) {
+            
+            session_start();
+            header("Location:Login.php");die;
+          }
+          else {
+            echo "nope";
+          }
+        }
+       
       }
     ?>
 	</body>
