f1e13c4ae5af6847e3242d6785b6d5cc51b48be8
youmna-salah <youmna_salah@icloud.com>
2015-10-06 06:21:54 +0200

    view histroy -profile info in progress

--- edit.php
+++ edit.php
@@ -11,19 +11,34 @@ mysql_connect ('localhost', 'root');
           
 
 if (isset($_POST['edit'])) {
-
+    $udoldemail =$_POST['oldemail'];
     $udfirstName = $_POST['first_name'];
     $udlastName = $_POST['last_name'];
     $udemail = $_POST['email'];
     $udpassword = $_POST['password'];
     $udavatar =isset($_POST['image'])?$_POST['image']:'';
+    // Required field names
+$required = array('oldemail', 'password', 'first_name', 'last_name', 'image', 'email');
+
+// Loop over field names, make sure each one exists and is not empty
+$error = false;
+foreach($required as $field) {
+  if (empty($_POST[$field])) {
+    $error = true;
+  }
+}
+
+if ($error) {
+  echo "<script>alert('All fields are required.');</script>";
+} else {
+  echo "<script>alert('Proceed...');</script>";
 	$query = "UPDATE `digonAlley`.`person` 
 	SET first_name ='$udfirstName'
 	,last_name ='$udlastName'
 	,image ='$udavatar'
 	,password= '$udpassword'
 	, email = '$udemail'
-	 WHERE id = 2 ";   			                
+	 WHERE email = '$udoldemail' ";   			                
 
 		    mysql_query($query)or die(mysql_error());
 	// if(FALSE. empty($query)){
@@ -38,7 +53,7 @@ if (isset($_POST['edit'])) {
 	}
 else
 {
-	echo "<script>alert('data is updated successfuly EL7AMDOULAAAAAAAAAA .....:D');</script>";
+	echo "<script>alert('data is updated successfuly :D');</script>";
 
 }
 	       if(array_key_exists('image', $_FILES)) {
@@ -50,13 +65,14 @@ else
               $upload_query = 'update person SET image = "'.$target_file.'" where email= "'.$_POST["email"].'";';
               $result = mysql_query($upload_query) or die(mysql_error());
               if($result) {
-              echo "image updated successfuly.";
+              echo "<script>alert('image updated successfuly.');</script>";
               }
               //echo "The file ". basename( $_POST["image"]). " has been uploaded.";
             } else {
-              echo "Sorry, there was an error uploading your file.";
+              echo "<script>alert('Sorry, there was an error uploading your file.');</script>";
             }
           }
+        }
 			}
             //echo implode("hhh ",$_FILES);
             // if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
@@ -80,7 +96,7 @@ else
 // }
 
 //}
- // echo '<META HTTP-EQUIV="Refresh" Content="0; URL=editinfo.php">'
+ echo '<META HTTP-EQUIV="Refresh" Content="0; URL=editinfo.php">'
 //redirect to location  page
 //header( 'Location: editinfo.php' ) ;
 
--- editInfo.php
+++ editInfo.php
@@ -17,24 +17,29 @@
           mysql_select_db('DigonAlley');
       ?>
       <form method="post" action="edit.php" enctype="multipart/form-data">
-<a><img align = "middle" src="http://fontmeme.com/embed.php?text=First-name&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+<a><img align = "middle" src="http://fontmeme.com/embed.php?text=First-name%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
 </br><input align ="middle" type="text" name="first_name" value="new first-name" /><br/>
 
-<a id="f"><img  align ="middle" src="http://fontmeme.com/embed.php?text=Last-name&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+<a id="f"><img  align ="middle" src="http://fontmeme.com/embed.php?text=Last-name%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
  </br><input type="text" align ="middle" name="last_name" value="new last-name"/><br />
 
-<a id="f"><img align ="middle" src="http://fontmeme.com/embed.php?text=Email*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+<a id="f"><img align ="middle" src="http://fontmeme.com/embed.php?text=OLd%20Email%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+</br><input align ="middle" type="text" name="oldemail" value="Old email"/><br />
+
+<a id="f"><img align ="middle" src="http://fontmeme.com/embed.php?text=Email%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
 </br><input align ="middle" type="text" name="email" value="new email"/><br />
 
-<a id="f"><img  align ="middle" src="http://fontmeme.com/embed.php?text=Password*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+<a id="f"><img  align ="middle" src="http://fontmeme.com/embed.php?text=Password%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
  </br><input  align ="middle" type="password" name="password" value="new password"/><br/>
 
- <a id="f"><img align ="middle" src="http://fontmeme.com/embed.php?text=Avatar&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+ <a id="f"><img align ="middle" src="http://fontmeme.com/embed.php?text=Avatar%20*&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
  </br><input  align ="middle" type="file" name="image" value="Browse" id="image"></br></br>
 <!-- <input type="file" name="image" value=""/><br /> -->
+<input align ="middle" id = "btn" type= "submit" name = "edit" value="Update">
 <a><img src="http://fontmeme.com/embed.php?text=*%20Required%20fields&name=HogwartsWizard.ttf&size=20&style_color=0C0C66" alt="Harry Potter Font"></a>
+</br>
+</br>
 
-<input type= "submit" name = "edit" value="Update">
 </br>
 </form>
 </h1>
