f19e88b7ae425b7849b2328c6cee8fd7ba732c61
shrfarzk <shrfarzk@gmail.com>
2015-10-07 21:57:23 +0200

    Site template

---
+++ __V2/view/template/headcontent.php
@@ -0,0 +1,3 @@
+<link rel="shortcut icon" href="img/fav/favicon.png">
+<link rel="stylesheet" type="text/css" href="css/normalize.css">
+<link rel="stylesheet" type="text/css" href="css/_common.css">
